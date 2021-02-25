5a7e1b3ad2830612a3e54b26d47a89afc7b3e45c
reckoner1429 <abhishekbisht1429@gmail.com>
2021-02-25 11:45:15 +0530

    Reset the cin and cout buffer after use in the template

--- template1.cpp
+++ template1.cpp
@@ -11,12 +11,17 @@ int main() {
     #ifdef DEBUG
     ifstream in("input.txt");
     ofstream out("output.txt");
-
+    streambuf* in_backup = cin.rdbuf();
+    streambuf* out_backup = cout.rdbuf();
     cin.rdbuf(in.rdbuf());
-    cout.rdbuf(out.rdbuf());
+    cout.rdbuf(out.rdbuf()); 
     #endif
 
-    
 
+
+    #ifdef DEBUG
+    cin.rdbuf(in_backup);
+    cout.rdbuf(out_backup);
+    #endif
     return 0;
 }
