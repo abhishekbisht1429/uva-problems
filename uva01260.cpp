3577d47d2a255c42d3b44f44d3abce6553153923
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-06-13 13:44:48 +0530

    uva01260 - Sales

---
+++ uva01260.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+
+#define _max3(a, b, c) max(a, max(b, c))
+#define _max4(a, b, c, d) max(max(a, b), max(c, d))
+#define _min3(a, b, c) min(a, min(b, c))
+#define _min4(a, b, c, d) min(min(a, b), min(c, d))
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) (n & (1L<<i))
+#define _set(n, i) n = (n | (1L<<i)) 
+#define _reset(n, i) n = (n & (0xfffffffffffffffeL<<i))
+#define _flip(n, i) n = (n ^ (1L<<i))
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+    #endif
+    /* ######################CODE_START################################ */
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; ++i) {
+            cin>>arr[i];
+        }
+        int sum = 0;
+        for(int i=1; i<n; ++i) {
+            for(int j=0; j<i; ++j) {
+                if(arr[j] <= arr[i])
+                    sum++;
+            }
+        }
+
+        cout<<sum<<"\n";
+    }
+
+    /* #######################CODE_END############################### */
+    #ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+    #endif
+    return 0;
+}
