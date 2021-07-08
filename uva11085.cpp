9980ed936b383c7a0827cfc62c1318616b670b4f
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-07-08 14:30:55 +0530

    uva11085 - Back to the 8-Queens

---
+++ uva11085.cpp
@@ -0,0 +1,106 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _uint unsigned int
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+#define _tp tuple
+#define _p pair
+
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) ((n) & (1u<<(i)))
+#define _set(n, i) n = (n | (1u<<(i))) 
+#define _reset(n, i) n = (n & ((-1u<<1)<<(i)))
+#define _flip(n, i) n = (n ^ (1u<<(i)))
+#define _rset(n, i, cnt) n = n | (((1u<<(cnt))-1)<<i)
+#define _rreset(n, i, cnt) n = n & ~(((1u<<(cnt))-1)<<i)
+#define _ffzi(n, i) __builtin_ffs(~(((1u<<i+1)-1) | n))
+
+#define _isSetl(n, i) ((n) & (1ull<<(i)))
+#define _setl(n, i) n = ((n) | (1ull<<(i))) 
+#define _resetl(n, i) n = ((n) & ((-1ull<<1)<<(i)))
+#define _flipl(n, i) n = ((n) ^ (1ull<<(i)))
+#define _rsetl(n, i, cnt) n = n | (((1ull<<(cnt))-1)<<i)
+#define _rresetl(n, i, cnt) n = n & ~(((1ull<<(cnt))-1)<<i)
+#define _ffzil(n, i) __builtin_ffs(~(((1ull<<i+1)-1) | n))
+
+_vec<_vec<int>> validPos;
+
+bool place(_vec<int> &row, int i, int q) {
+    for(int j=0; j<q; ++j) {
+        if(row[j] == i || abs(q - j) == abs(i - row[j]))
+            return false;
+    }
+    return true;
+}
+void findValidPos(_vec<int> &row, int q=0) {
+    if(q>7) {
+        validPos.push_back(row);
+        return;
+    }
+
+    for(int i=0; i<8; ++i) {
+        if(place(row, i, q)) {
+            row[q] = i;
+            findValidPos(row, q+1);
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+    #endif
+
+    /* ######################CODE_START################################ */
+    _vec<int> temp(8);
+    findValidPos(temp);
+
+    int tc = 0;
+    while(1) {
+        _vec<int> row(8);
+        for(int j=0; j<8; ++j) {
+            cin>>row[j];
+            row[j]--;
+        }
+        if(cin.fail()) break;
+
+        int min_moves = INT_MAX;
+        for(auto validRow : validPos) {
+            int moves = 0;
+            for(int j=0; j<8; ++j) {
+                moves += (validRow[j] - row[j]) != 0;
+            }
+            min_moves = min(min_moves, moves);
+        }
+        cout<<"Case "<<(++tc)<<": "<<min_moves<<"\n";
+    }
+
+
+    /* #######################CODE_END############################### */
+    #ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+    #endif
+    return 0;
+}
