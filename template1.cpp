e0e285fd493fa273c88a12d84cd4a3f617f88c5b
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-07-30 19:45:29 +0530

    added content to template

--- template1.cpp
+++ template1.cpp
@@ -1,27 +1,67 @@
 #include<bits/stdc++.h>
-#define DEBUG
 
 using namespace std;
 
-void ignore_lines(int n) {
-    while(n--)
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _uint unsigned int
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+#define _tp tuple
+#define _p pair
+
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) ((n) & (1u<<(i)))
+#define _set(n, i) n = (n | (1u<<(i))) 
+#define _reset(n, i) n = (n & (~(1u<<(i))))
+#define _flip(n, i) n = (n ^ (1u<<(i)))
+#define _rset(n, i, cnt) n = n | (((1u<<(cnt))-1)<<i)
+#define _rreset(n, i, cnt) n = n & ~(((1u<<(cnt))-1)<<i)
+#define _ffzi(n, i) __builtin_ffs(~(((1u<<i+1)-1) | n))
+
+#define _isSetl(n, i) ((n) & (1ull<<(i)))
+#define _setl(n, i) n = ((n) | (1ull<<(i))) 
+#define _resetl(n, i) n = (n & (~(1ull<<(i))))
+#define _flipl(n, i) n = ((n) ^ (1ull<<(i)))
+#define _rsetl(n, i, cnt) n = n | (((1ull<<(cnt))-1)<<i)
+#define _rresetl(n, i, cnt) n = n & ~(((1ull<<(cnt))-1)<<i)
+#define _ffzil(n, i) __builtin_ffs(~(((1ull<<i+1)-1) | n))
+
+typedef _p<int, int> _ii;
+
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     #ifdef DEBUG
-    ifstream in("input.txt");
-    ofstream out("output.txt");
-    streambuf* in_backup = cin.rdbuf();
-    streambuf* out_backup = cout.rdbuf();
-    cin.rdbuf(in.rdbuf());
-    cout.rdbuf(out.rdbuf()); 
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
     #endif
+    /* ######################CODE_START################################ */
 
 
 
+
+
+
+    /* #######################CODE_END############################### */
     #ifdef DEBUG
-    cin.rdbuf(in_backup);
-    cout.rdbuf(out_backup);
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
     #endif
     return 0;
 }
