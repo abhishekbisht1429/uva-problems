edc0a5f66a384951aeb459e7c3ba88e3f7c6489a
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-07-31 17:01:31 +0530

    uva00983 - Localized Summing for Blurring

---
+++ uva00983.cpp
@@ -0,0 +1,101 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _uint unsigned int
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+#define _tp tuple
+#define _p pair
+
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) ((n) & (1u<<(i)))
+#define _set(n, i) n = (n | (1u<<(i))) 
+#define _reset(n, i) n = (n & (~(1u<<(i))))
+#define _flip(n, i) n = (n ^ (1u<<(i)))
+#define _rset(n, i, cnt) n = n | (((1u<<(cnt))-1)<<i)
+#define _rreset(n, i, cnt) n = n & ~(((1u<<(cnt))-1)<<i)
+#define _ffzi(n, i) __builtin_ffs(~(((1u<<i+1)-1) | n))
+
+#define _isSetl(n, i) ((n) & (1ull<<(i)))
+#define _setl(n, i) n = ((n) | (1ull<<(i))) 
+#define _resetl(n, i) n = (n & (~(1ull<<(i))))
+#define _flipl(n, i) n = ((n) ^ (1ull<<(i)))
+#define _rsetl(n, i, cnt) n = n | (((1ull<<(cnt))-1)<<i)
+#define _rresetl(n, i, cnt) n = n & ~(((1ull<<(cnt))-1)<<i)
+#define _ffzil(n, i) __builtin_ffs(~(((1ull<<i+1)-1) | n))
+
+typedef _p<int, int> _ii;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+    #endif
+    /* ######################CODE_START################################ */
+    int tc = 0;
+    while(1) {
+        
+        int n, m;
+        cin>>n>>m;
+        if(cin.fail()) break;
+        if(tc++) cout<<"\n";
+
+        _vec<_vec<_ll>> cum_mat(n, _vec<_ll>(n));
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<n; ++j) {
+                cin>>cum_mat[i][j];
+
+                if(i>0) cum_mat[i][j] += cum_mat[i-1][j];
+                if(j>0) cum_mat[i][j] += cum_mat[i][j-1];
+                if(i>0 && j>0) cum_mat[i][j] -= cum_mat[i-1][j-1];
+            }
+        }
+
+        _ll total_sum = 0;
+        for(int i1=0; i1+m-1<n; ++i1) {
+            for(int j1=0; j1+m-1<n; ++j1) {
+                int i2 = i1+m-1, j2 = j1+m-1;
+
+                _ll sum = cum_mat[i2][j2];
+                if(i1>0) sum -= cum_mat[i1-1][j2];
+                if(j1>0) sum -= cum_mat[i2][j1-1];
+                if(i1>0 && j1>0) sum += cum_mat[i1-1][j1-1];
+
+                cout<<sum<<"\n";
+                total_sum += sum;
+            }
+        }
+        cout<<total_sum<<"\n";
+    }
+
+
+
+
+
+    /* #######################CODE_END############################### */
+    #ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+    #endif
+    return 0;
+}
