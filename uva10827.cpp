99b4095341327d3488275e0968c4a58d3a42242a
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-08-04 21:16:42 +0530

    uva10827 - Maximum sum on a Torus

---
+++ uva10827.cpp
@@ -0,0 +1,102 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _uint unsigned int
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+#define _tp tuple
+#define _p pair
+
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) ((n) & (1u<<(i)))
+#define _set(n, i) n = (n | (1u<<(i)))
+#define _reset(n, i) n = (n & (~(1u<<(i))))
+#define _flip(n, i) n = (n ^ (1u<<(i)))
+#define _rset(n, i, cnt) n = n | (((1u<<(cnt))-1)<<i)
+#define _rreset(n, i, cnt) n = n & ~(((1u<<(cnt))-1)<<i)
+#define _ffzi(n, i) __builtin_ffs(~(((1u<<i+1)-1) | n))
+
+#define _isSetl(n, i) ((n) & (1ull<<(i)))
+#define _setl(n, i) n = ((n) | (1ull<<(i)))
+#define _resetl(n, i) n = (n & (~(1ull<<(i))))
+#define _flipl(n, i) n = ((n) ^ (1ull<<(i)))
+#define _rsetl(n, i, cnt) n = n | (((1ull<<(cnt))-1)<<i)
+#define _rresetl(n, i, cnt) n = n & ~(((1ull<<(cnt))-1)<<i)
+#define _ffzil(n, i) __builtin_ffs(~(((1ull<<i+1)-1) | n))
+
+typedef _p<int, int> _ii;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+    #endif
+    /* ######################CODE_START################################ */
+    int tc;
+    cin>>tc;
+    while(tc--) {
+        int n;
+        cin>>n;
+        _vec<_vec<int>> torus(n, _vec<int>(n));
+        for(int i=0; i<n; ++i) {
+            for(int j=0; j<n; ++j) {
+                cin>>torus[i][j];
+            }
+        }
+
+        _vec<int> squash(n);
+        int max_s = 0;
+        for(int i1=0; i1<n; ++i1) {
+            squash.assign(n, 0);
+            for(int i2=i1; i2<n || (i2%n)<i1; ++i2) {
+                for(int j=0; j<n; ++j) 
+                    squash[j] += torus[i2%n][j];
+                
+                int total = accumulate(squash.begin(), squash.end(), 0);
+                int sum = 0, max_squash_sum = INT_MIN;
+                for(int j=0; j<n; ++j) {
+                    sum += squash[j];
+                    max_squash_sum = max(max_squash_sum, sum);
+                    sum = max(0, sum);
+                }
+                sum = 0;
+                for(int j=0; j<n; ++j) {
+                    sum += squash[j];
+                    max_squash_sum = max(max_squash_sum, total - sum);
+                    sum = min(0, sum);
+                }
+                
+                max_s = max(max_s, max_squash_sum);
+            }
+        }
+
+        cout<<max_s<<"\n";
+    }
+
+    /* #######################CODE_END############################### */
+    #ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+    #endif
+    return 0;
+}
