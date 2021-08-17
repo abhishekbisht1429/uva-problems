6972f6ffa003e2bbc9355d76743c0c4305645158
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-08-17 19:52:39 +0530

    uva00562_v2 - Dividing Coins (knapsack - top down)

---
+++ uva00562_v2.cpp
@@ -0,0 +1,99 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _uint unsigned int
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+#define _tp tuple
+#define _p pair
+
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) ((n) & (1u<<(i)))
+#define _set(n, i) n = (n | (1u<<(i))) 
+#define _reset(n, i) n = (n & (~(1u<<(i))))
+#define _flip(n, i) n = (n ^ (1u<<(i)))
+#define _rset(n, i, cnt) n = n | (((1u<<(cnt))-1)<<i)
+#define _rreset(n, i, cnt) n = n & ~(((1u<<(cnt))-1)<<i)
+#define _ffzi(n, i) __builtin_ffs(~(((1u<<i+1)-1) | n))
+
+#define _isSetl(n, i) ((n) & (1ull<<(i)))
+#define _setl(n, i) n = ((n) | (1ull<<(i))) 
+#define _resetl(n, i) n = (n & (~(1ull<<(i))))
+#define _flipl(n, i) n = ((n) ^ (1ull<<(i)))
+#define _rsetl(n, i, cnt) n = n | (((1ull<<(cnt))-1)<<i)
+#define _rresetl(n, i, cnt) n = n & ~(((1ull<<(cnt))-1)<<i)
+#define _ffzil(n, i) __builtin_ffs(~(((1ull<<i+1)-1) | n))
+
+typedef _p<int, int> _ii;
+
+int m;
+_vec<int> coins;
+_vec<_vec<int>> dp;
+int total;
+
+/* top down approach */
+int findOptSol(int s=total/2, int i=0) {
+    if(i==m)
+        return 0;
+
+    if(dp[s][i] == -1) {
+        int v1, v2 = 0;
+        v1 = findOptSol(s, i+1);
+        if(s - coins[i] >= 0)
+            v2 = coins[i] + findOptSol(s-coins[i], i+1);
+        
+        dp[s][i] = max(v1, v2);
+    }
+
+    return dp[s][i];
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+    #endif
+    /* ######################CODE_START################################ */
+    int tc;
+    cin>>tc;
+    while(tc--) {
+        cin>>m;
+        coins.resize(m);
+        total = 0;
+        for(int i=0; i<m; ++i) {
+            cin>>coins[i];
+            total += coins[i];
+        }
+        dp.resize(total/2+1);
+        dp.assign(total/2+1, _vec<int>(m, -1)); // for top down approach
+
+        cout<<abs(total - 2*findOptSol())<<"\n";
+    }
+
+    /* #######################CODE_END############################### */
+    #ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+    #endif
+    return 0;
+}
