385345aebe6cf20f51e0acb7f46b7988bdf3b665
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-12-25 13:13:05 +0530

    uva01062 - Containers

---
+++ uva01062.cpp
@@ -0,0 +1,92 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _uint unsigned int
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+#define _tp tuple
+#define _p pair
+
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) ((n) & (1u<<(i)))
+#define _set(n, i) n = (n | (1u<<(i))) 
+#define _reset(n, i) n = (n & (~(1u<<(i))))
+#define _flip(n, i) n = (n ^ (1u<<(i)))
+#define _rset(n, i, cnt) n = n | (((1u<<(cnt))-1)<<i)
+#define _rreset(n, i, cnt) n = n & ~(((1u<<(cnt))-1)<<i)
+#define _ffzi(n, i) __builtin_ffs(~(((1u<<i+1)-1) | n))
+
+#define _isSetl(n, i) ((n) & (1ull<<(i)))
+#define _setl(n, i) n = ((n) | (1ull<<(i))) 
+#define _resetl(n, i) n = (n & (~(1ull<<(i))))
+#define _flipl(n, i) n = ((n) ^ (1ull<<(i)))
+#define _rsetl(n, i, cnt) n = n | (((1ull<<(cnt))-1)<<i)
+#define _rresetl(n, i, cnt) n = n & ~(((1ull<<(cnt))-1)<<i)
+#define _ffzil(n, i) __builtin_ffs(~(((1ull<<i+1)-1) | n))
+
+typedef _p<int, int> _ii;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+    #endif
+    /* ######################CODE_START################################ */
+    int tc = 0;
+    while(1) {
+        string s;
+        cin>>s;
+        if(s == "end")
+            break;
+        cout<<"Case "<<(++tc)<<": ";
+        _vec<char> stack_tops;
+
+        for(char c : s) {
+            int pos = -1;
+            for(int i=0; i<stack_tops.size(); ++i) {
+                if(c <= stack_tops[i]) {
+                    pos = i;
+                    break;
+                }
+            }
+
+            if(pos == -1)
+                stack_tops.push_back(c);
+            else
+                stack_tops[pos] = c;
+        }
+
+        cout<<stack_tops.size()<<"\n";
+    }
+
+
+
+
+
+    /* #######################CODE_END############################### */
+    #ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+    #endif
+    return 0;
+}
