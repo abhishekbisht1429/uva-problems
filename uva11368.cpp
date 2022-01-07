37dd51a49b7425a7be27fc1ad1fef3fb9506c0f0
Abhishek Bisht <abhishekbisht1429@gmail.com>
2022-01-07 20:09:53 +0530

    uva11368 - Nested Dolls

---
+++ uva11368.cpp
@@ -0,0 +1,106 @@
+//
+// Created by reckoner1429 on 06/01/22.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _uint unsigned int
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+#define _tp tuple
+#define _p pair
+
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) ((n) & (1u<<(i)))
+#define _set(n, i) n = (n | (1u<<(i)))
+#define _reset(n, i) n = (n & (~(1u<<(i))))
+#define _flip(n, i) n = (n ^ (1u<<(i)))
+#define _rset(n, i, cnt) n = n | (((1u<<(cnt))-1)<<i)
+#define _rreset(n, i, cnt) n = n & ~(((1u<<(cnt))-1)<<i)
+#define _ffzi(n, i) __builtin_ffs(~(((1u<<i+1)-1) | n))
+
+#define _isSetl(n, i) ((n) & (1ull<<(i)))
+#define _setl(n, i) n = ((n) | (1ull<<(i)))
+#define _resetl(n, i) n = (n & (~(1ull<<(i))))
+#define _flipl(n, i) n = ((n) ^ (1ull<<(i)))
+#define _rsetl(n, i, cnt) n = n | (((1ull<<(cnt))-1)<<i)
+#define _rresetl(n, i, cnt) n = n & ~(((1ull<<(cnt))-1)<<i)
+#define _ffzil(n, i) __builtin_ffs(~(((1ull<<i+1)-1) | n))
+
+typedef _p<int, int> _ii;
+
+bool comp(_ii &p1, _ii &p2) {
+    if(p1.first == p2.first)
+        return p1.second < p2.second;
+    return p1.first > p2.first;
+}
+
+int lis(_vec<int> &vec) {
+    _vec<int> l_vec;
+    for(int i=0; i<vec.size(); ++i) {
+        int pos = upper_bound(l_vec.begin(), l_vec.end(), vec[i]) -
+                l_vec.begin();
+        if(pos == l_vec.size())
+            l_vec.push_back(vec[i]);
+        else
+            l_vec[pos] = vec[i];
+    }
+
+    return l_vec.size();
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+#endif
+    /* ######################CODE_START################################ */
+    int t;
+    cin>>t;
+    while(t--) {
+        int m;
+        cin>>m;
+        _vec<_ii> vec(m);
+        for(int i=0; i<m; ++i)
+            cin>>vec[i].first>>vec[i].second;
+
+        sort(vec.begin(), vec.end(), comp);
+
+        _vec<int> heights;
+        for(int i=0; i<vec.size(); ++i)
+            heights.push_back(vec[i].second);
+
+        cout<<lis(heights)<<"\n";
+    }
+
+
+
+
+
+    /* #######################CODE_END############################### */
+#ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+#endif
+    return 0;
+}
