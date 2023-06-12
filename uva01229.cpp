b3f4588edf715bc9c045f29144bcbdae79990565
Abhishek Bisht <abhishekbisht1429@gmail.com>
2023-06-12 23:38:39 +0530

    uva01229 - Sub-dictionary

---
+++ uva01229.cpp
@@ -0,0 +1,154 @@
+//
+// Created by abhishek on 9/6/23.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define DEBUG
+
+#define _umap unordered_map
+#define _uset unordered_set
+#define _uint unsigned int
+#define _ll long long
+#define _ull unsigned _ll
+#define _vec vector
+#define _tp tuple
+#define _p pair
+
+#define _ignore_lines(n) for(int i=0;i<n;++i) cin.ignore(numeric_limits<streamsize>::max(), '\n')
+
+/* Bit Manipulation */
+#define _isSet(n, i) ((n) & (1u<<(i)))
+#define _set(n, i) n = (n | (1u<<(i)))
+#define _reset(n, i) n = (n & (~(1u<<(i))))
+#define _flip(n, i) n = (n ^ (1u<<(i)))
+#define _rset(n, i, cnt) n = n | (((1u<<(cnt))-1)<<i)
+#define _rreset(n, i, cnt) n = n & ~(((1u<<(cnt))-1)<<i)
+#define _ffzi(n, i) __builtin_ffs(~(((1u<<i+1)-1) | n))
+
+#define _isSetl(n, i) ((n) & (1ull<<(i)))
+#define _setl(n, i) n = ((n) | (1ull<<(i)))
+#define _resetl(n, i) n = (n & (~(1ull<<(i))))
+#define _flipl(n, i) n = ((n) ^ (1ull<<(i)))
+#define _rsetl(n, i, cnt) n = n | (((1ull<<(cnt))-1)<<i)
+#define _rresetl(n, i, cnt) n = n & ~(((1ull<<(cnt))-1)<<i)
+#define _ffzil(n, i) __builtin_ffs(~(((1ull<<i+1)-1) | n))
+
+typedef _p<int, int> _ii;
+
+void solve(map<int, set<int>>& adj_list, map<int, set<int>>& adj_list_t) {
+    bool done = false;
+    vector<bool> removed(adj_list_t.size(), false);
+    while(!done) {
+        done = true;
+        for(auto itr=adj_list_t.begin(); itr != adj_list_t.end(); ++itr) {
+            int u = itr->first;
+            if(removed[u])
+                continue;
+            if(itr->second.empty()) {
+                removed[u] = true;
+                for(auto& p : adj_list_t) {
+                    if(p.second.find(u) != p.second.end()) {
+                        p.second.erase(u);
+                    }
+                }
+                for(auto& p : adj_list) {
+                    if(p.second.find(u) != p.second.end()) {
+                        p.second.erase(u);
+                    }
+                }
+                adj_list.erase(u);
+                done = false;
+            }
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+#endif
+    /* ######################CODE_START################################ */
+    while(1) {
+        int n;
+        cin>>n;
+        if(n==0)
+            break;
+        _ignore_lines(1);
+        map<int, set<int>> adj_list;
+        map<int, set<int>> adj_list_t;
+        for(int i=0; i<n; ++i) {
+            adj_list[i] = set<int>();
+            adj_list_t[i] = set<int>();
+        }
+        vector<string> words(n);
+        unordered_map<string, int> indices;
+        int index = 0;
+        for(int i=0; i<n; ++i) {
+            string line;
+            getline(cin, line);
+            istringstream iss(line);
+
+            string u;
+            iss>>u;
+            if(indices.find(u) == indices.end()) {
+                indices[u] = index;
+                words[index++] = u;
+            }
+            while(1) {
+                string v;
+                iss>>v;
+                if(iss.fail())
+                    break;
+                if(indices.find(v) == indices.end()) {
+                    indices[v] = index;
+                    words[index++] = v;
+                }
+
+                adj_list[indices[u]].insert(indices[v]);
+                adj_list_t[indices[v]].insert(indices[u]);
+            }
+        }
+
+        solve(adj_list, adj_list_t);
+        vector<string> res;
+        for(auto& p : adj_list) {
+            res.push_back(words[p.first]);
+        }
+
+        sort(res.begin(), res.end());
+
+        cout<<res.size()<<"\n";
+        for(int i=0; i<res.size(); ++i) {
+            if(i!=0)
+                cout<<" ";
+            cout<<res[i];
+        }
+        cout<<"\n";
+    }
+
+
+
+
+
+    /* #######################CODE_END############################### */
+#ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    _ll _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+#endif
+    return 0;
+}
